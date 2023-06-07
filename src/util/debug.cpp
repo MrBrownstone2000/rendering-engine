10f45b7e6e360d3746bafcfec78378c87acf575c
Célian Louis-Itty <celian.louis-itty@etu.univ-lyon1.fr>
2023-06-08 00:06:05 +0200

    Fixed memory leak in demangle code

--- src/util/debug.cpp
+++ src/util/debug.cpp
@@ -5,8 +5,11 @@
 
 namespace engine::util
 {
-    const char* Demangle(const char* name)
+    std::string Demangle(const char* name)
     {
-        return abi::__cxa_demangle(name, nullptr, nullptr, nullptr);
+        char* buf = abi::__cxa_demangle(name, nullptr, nullptr, nullptr);
+        std::string res = buf != nullptr ? buf : name;
+        std::free(buf);
+        return res;
     }
 }
--- src/util/debug.hpp
+++ src/util/debug.hpp
@@ -1,9 +1,11 @@
 #ifndef __DEBUG_HPP__
 #define __DEBUG_HPP__
 
+#include <string>
+
 namespace engine::util
 {
-    const char* Demangle(const char* name);
+    std::string Demangle(const char* name);
 }
 
 #endif
