b9471610f9e417268a9501eacea9ecd0dc766b11
Célian Louis-Itty <celian.louis-itty@etu.univ-lyon1.fr>
2023-07-02 00:01:23 +0200

    Print Opengl and driver informations on context creation

--- src/renderer/openGLContext.cpp
+++ src/renderer/openGLContext.cpp
@@ -1,5 +1,6 @@
 #include "pch.hpp"
 #include "openGLContext.hpp"
+#include "GL/glew.h"
 
 namespace engine::renderer
 {
@@ -8,6 +9,12 @@ namespace engine::renderer
     {
         m_context = SDL_GL_CreateContext(m_window);
         Check(m_context).msg("Error: [GL] failed to get GL context from window");
+        std::stringstream ss;
+        ss << "OpenGL Context:" << std::endl;
+        ss << "    Vendor: " << glGetString(GL_VENDOR) << std::endl;
+        ss << "    Renderer: " << glGetString(GL_RENDERER) << std::endl;
+        ss << "    Version: " << glGetString(GL_VERSION) << std::endl;
+        engineLog.info(ss.str());
     }
 
     OpenGLContext::~OpenGLContext()
